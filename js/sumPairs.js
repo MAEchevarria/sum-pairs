7e0511eed9a57f47f4343917f15e57863a933bc6
Michael Echevarria <michael.echevarria@live.com>
2022-05-26 16:13:45 -0500

    updated code for js

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,17 @@
-exports.sumPairs = function() {
-
+exports.sumPairs = function(arr, sum) {
+    let newArr = arr;
+    let pairArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < newArr.length; j++) {
+            if (arr[i] + newArr[j] === sum) {
+                pairArr.push([arr[i], newArr[j]])
+                newArr.pop(newArr[j]);
+            }
+        }
+    }
+    if (pairArr.length === 0) {
+        return "unable to find pairs";
+    } else {
+        return pairArr;
+    }
 };
